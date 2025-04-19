b6df0ff11cb854da2a9d5c7181434caec6397fca
Ryu-ga <frech5679@icloud.com>
2025-04-20 01:39:34 +0900

    Fix ApiClient.ts

--- src/apis/common/ApiClient.ts
+++ src/apis/common/ApiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const service = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.API_BASE_URL,
   headers: {
     "Cache-Control": "no-cache",
   }
