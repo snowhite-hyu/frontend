1936aecf33dfb4651e837e621c7689599687b253
choiao0 <qawse6@daum.net>
2025-05-13 02:45:06 +0900

    refactor : Dropdown 컴포넌트화

--- src/pages/CreateRoom.tsx
+++ src/pages/CreateRoom.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Dropdown } from "@/components/ui/Dropdown";
 // import useLogin from "@/hooks/useLogin";
 import type { CreateRoomRequest } from "@/models/common/Room";
 import update from "immutability-helper";
@@ -36,40 +37,25 @@ const CreateRoomPage: React.FC = () => {
 						setForm(update(form, { roomName: { $set: e.target.value } }))
 					}
 				/>
-                <div className="flex justify-between items-center">
-                    <div className="text-3xl font-semibold text-white">인원 선택</div>
-					<select 
-                        className="bg-white text-black rounded-lg h-12 min-w-[111px] text-center text-2xl"
-                        value={form.maxPlayers}
-                        onChange={(e) =>
-                            setForm(update(form, { maxPlayers: { $set: Number(e.target.value) } }))
-                        }
-                        required
-                    >
-						{[3, 4, 5, 6, 7, 8, 9, 10].map((value) => (
-                            <option key={value} value={value}>
-                                {value}
-                            </option>
-                        ))}
-					</select>
-				</div>
-                <div className="flex justify-between items-center">
-                    <div className="text-3xl font-semibold text-white">턴 시간 제한</div>
-					<select 
-                        className="bg-white text-black rounded-lg h-12 min-w-[147px] text-center text-2xl"
-                        value={form.turnTimeLimit}
-                        onChange={(e) =>
-                            setForm(update(form, { turnTimeLimit: { $set: Number(e.target.value) } }))
-                        }
-                        required
-                    >
-						{[15, 30, 45, 60].map((value) => (
-                            <option key={value} value={value}>
-                                {value}초
-                            </option>
-                        ))}
-					</select>
-				</div>
+				<Dropdown
+					label="인원 선택"
+					value={form.maxPlayers}
+					options={[3, 4, 5, 6, 7, 8, 9, 10]}
+					onChange={(value) =>
+						setForm(update(form, { maxPlayers: { $set: value } }))
+					}
+					className="min-w-[111px]"
+				/>
+				<Dropdown
+					label="턴 시간 제한"
+					value={form.turnTimeLimit}
+					options={[15, 30, 45, 60]}
+					onChange={(value) =>
+						setForm(update(form, { turnTimeLimit: { $set: value } }))
+					}
+					className="min-w-[147px]"
+					optionSuffix="초"
+				/>
 				<Button variant={"sabotuer"} className="w-fit h-fit" type="submit">
 					Create
 				</Button>
