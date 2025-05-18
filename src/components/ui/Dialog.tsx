599f3b3efbd5ffcd1b03fa583b5fedf193a5eabf
choiao0 <qawse6@daum.net>
2025-05-18 16:09:17 +0900

    feat: Dialog 컴포넌트 구현

---
+++ src/components/ui/Dialog.tsx
@@ -0,0 +1,67 @@
+import { ReactNode } from 'react';
+import { 
+  useFloating, 
+  useDismiss, 
+  useRole, 
+  useInteractions, 
+  useId, 
+  FloatingOverlay, 
+  FloatingFocusManager
+} from '@floating-ui/react';
+
+interface DialogProps {
+  size?: 'big' | 'small';
+  children: ReactNode;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+const widthMap = {
+  big: 'w-[1000px]',
+  small: 'w-[600px]',
+};
+
+const Dialog: React.FC<DialogProps> = ({ size = 'big', children, isOpen, setIsOpen }) => {
+  const { refs, context } = useFloating({
+    open: isOpen,
+    onOpenChange: setIsOpen,
+  });
+
+  const dismiss = useDismiss(context, {
+    outsidePressEvent: 'mousedown',
+  });
+  const role = useRole(context);
+  
+  const { getFloatingProps } = useInteractions([
+    dismiss, 
+    role,
+  ]);
+
+  const labelId = useId();
+  const descriptionId = useId();
+
+  return (
+    <>
+      {isOpen && (
+        <FloatingOverlay
+          lockScroll
+          className="grid place-items-center z-10"
+        >
+          <FloatingFocusManager context={context}>
+            <div
+              className={`${widthMap[size]} bg-black/70 place-items-center m-4 p-4 rounded-4xl h-[600px] flex flex-col items-center text-center`}
+              ref={refs.setFloating}
+              aria-labelledby={labelId}
+              aria-describedby={descriptionId}
+              {...getFloatingProps()}
+            >
+              {children}
+            </div>
+          </FloatingFocusManager>
+        </FloatingOverlay>
+      )}
+    </>
+  );
+};
+
+export default Dialog;
