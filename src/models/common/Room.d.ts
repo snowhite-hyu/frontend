e6a3f59686aa4e0e24cb7ec7998734d25a874313
iyeongseo <iyeongssseo@gmail.com>
2025-05-25 02:35:20 +0900

    feat : 방 목록 조회 비즈니스 로직 구현
    - api 응답 타입 백엔드 확인 후 수정 필요

--- src/models/common/Room.d.ts
+++ src/models/common/Room.d.ts
@@ -11,3 +11,15 @@ export interface CreateRoomResponse {
 	maxPlayers?: number;
 	turnTimeLimit?: number;
 }
+
+// 각 속성 타입 확인 필요
+export interface RoomItem {
+	roomId: string;
+	masterPlayer: string;
+	users: string;
+	capacity: number;
+	turnTime: number;
+	isPlaying: boolean;
+}
+
+export type ListResponse = RoomItem[];
