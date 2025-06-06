37e18d6e6a7de6a71924be6918f5643d0cf1dce5
iyeongseo <iyeongssseo@gmail.com>
2025-06-06 21:30:52 +0900

    feat : 새로고침 버튼 추가 및 방 목록 스크롤 설정

--- src/pages/RoomList.tsx
+++ src/pages/RoomList.tsx
@@ -55,38 +55,50 @@ const RoomListPage: React.FC = () => {
 		<div className="flex flex-inline w-full h-full items-start">
 			<div className="flex flex-col w-full mt-[10%] mr-[10%] ml-auto">
 				<div className="w-full max-w-[500px] min-w-[300px] h-[339px] rounded-[21.87px] bg-[#ffffff] mr-0 ml-auto p-[30px]">
-					{rooms.length === 0 ? (
-						<div className="w-full h-full flex justify-center items-center">
-							<p className="text-[#999999] text-[20px] text-center">방이 없습니다.</p>
-						</div>
-					) : (
-					rooms.map((room) => (
-						<div
-							key={room.roomId}
-							className="w-full flex flex-inline mb-[10px] items-center "
-						>
-							<p className="text-[#000000] text-[25px] ml-[0%] mr-auto max-w-[350px] overflow-hidden">
-								{room.roomId}
-							</p>
-							<Button
-								variant={"saboteurCheck"}
-								className="h-fit mr-[0%] ml-auto text-[21px]"
-								onClick={() => handleJoinRoom(room.roomId)}
+					<div className="h-full overflow-y-auto">
+						{rooms.length === 0 ? (
+							<div className="w-full h-full flex justify-center items-center">
+								<p className="text-[#999999] text-[20px] text-center">방이 없습니다.</p>
+							</div>
+						) : (
+						rooms.map((room) => (
+							<div
+								key={room.roomId}
+								className="w-full flex flex-inline mb-[10px] items-center "
 							>
-								참가
-							</Button>
-						</div>
-					))
-					)}
+								<p className="text-[#000000] text-[25px] ml-[0%] mr-auto max-w-[350px] overflow-hidden">
+									{room.roomId}
+								</p>
+								<Button
+									variant={"saboteurCheck"}
+									className="h-fit mr-[0%] ml-auto text-[21px]"
+									onClick={() => handleJoinRoom(room.roomId)}
+								>
+									참가
+								</Button>
+							</div>
+						))
+						)}
+					</div>
 				</div>
-				<Button
-					variant={"sabotuer"}
-					className="w-fit h-fit mt-[3%] mr-[0%] ml-auto"
-					type="submit"
-					onClick={() => navigate("/create-room")}
-				>
-					생성
-				</Button>
+				<div className="flex w-[500px] justify-between mt-[3%] mr-0 ml-auto">
+					<Button
+						variant={"saboteurCheck"}
+						className="w-fit h-fit bg-[#E0E0E0] text-[#B22222] font-extrabold"
+						type="submit"
+						onClick={() => window.location.reload()}
+					>
+						새로고침
+					</Button>
+					<Button
+						variant={"sabotuer"}
+						className="w-fit h-fit"
+						type="submit"
+						onClick={() => navigate("/create-room")}
+					>
+						생성
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
