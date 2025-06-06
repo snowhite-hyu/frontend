a11606830ebac8c83dcdf366e48ddc1b49d02e6d
iyeongseo <iyeongssseo@gmail.com>
2025-06-06 21:57:50 +0900

    remove : 주석 삭제

--- src/stores/common/RoomSocketStore.ts
+++ src/stores/common/RoomSocketStore.ts
@@ -1,34 +1,3 @@
-// import { create } from "zustand";
-// import { RoomSocket } from "@/sockets/RoomSocket";
-
-// interface RoomSocketState {
-//     socket: RoomSocket | null;
-//     connect: (jwtToken: string) => void;
-//     disconnect: () => void;
-// }
-
-// export const useRoomSocketStore = create<RoomSocketState>((set, get) => ({
-//     socket: null,
-
-//     connect: (jwtToken: string) => {
-//         const existingSocket = get().socket;
-//         if(existingSocket) {
-//             existingSocket?.disconnect(); // 기존 소켓 연결 해제
-//         }
-//         const socket = new RoomSocket(jwtToken);
-//         set({ socket: socket });
-//     },
-    
-//     disconnect: () => {
-//         const socket = get().socket;
-//         if (socket) {
-//             socket.disconnect();
-//             set({ socket: null });
-//         }
-//     },
-
-// }));
-
 import { create } from "zustand";
 import { RoomSocket } from "@/sockets/RoomSocket";
 
