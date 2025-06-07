4b1d90642643eaf6b576e48aac35fc8d903301a8
Ryu-ga <frech5679@icloud.com>
2025-06-07 23:23:19 +0900

    룸 상태 통일

--- src/stores/common/RoomInfoState.ts
+++ src/stores/common/RoomInfoState.ts
@@ -1,18 +1,22 @@
 import { create } from "zustand";
-import { WsRoomUserModel } from "@/models/common/User";
 import { RoomItem } from "@/models/common/Room";
+import update from "immutability-helper";
 
 interface RoomInfoState {
 	room: RoomItem | null;
-	setRoom: (room: any) => void;
-	updateUsers: (users: WsRoomUserModel[]) => void;
+	isMaster: boolean;
+	setRoom: (room: RoomInfoState["room"]) => void;
+	updateUsers: (users: RoomItem["users"]) => void;
 	clearRoom: () => void;
+	setIsMaster: (value: RoomInfoState["isMaster"]) => void;
 }
 
 export const useRoomInfoStore = create<RoomInfoState>((set) => ({
 	room: null,
+	isMaster: false,
+	gameId: null,
 	setRoom: (room) => set({ room }),
-	updateUsers: (users) =>
-		set((state) => (state.room ? { room: { ...state.room, users } } : {})),
+	updateUsers: (users) => set((state) => state.room ? update(state, { room: { users: { $set: users } } }) : state),
 	clearRoom: () => set({ room: null }),
+	setIsMaster: (value) => set({ isMaster: value }),
 }));
