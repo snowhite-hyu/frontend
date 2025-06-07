aaa7a39443757a37b1cda696d769cb381d163f4a
Ryu-ga <frech5679@icloud.com>
2025-06-08 04:49:26 +0900

    채팅 기능 구현

--- src/models/common/Room.d.ts
+++ src/models/common/Room.d.ts
@@ -27,3 +27,17 @@ export interface RoomItem {
 export type ListResponse = {
 	roomList: RoomItem[];
 }
+
+export interface ChatRequest {
+	roomId: number;
+	message: string;
+}
+
+export interface ChatResponse {
+	user: {
+		id: number;
+		username: string;
+		loggedIn: boolean;
+	};
+	message: string;
+}
--- src/stores/common/RoomInfoState.ts
+++ src/stores/common/RoomInfoState.ts
@@ -1,17 +1,35 @@
 import { create } from "zustand";
+import update from "immutability-helper";
 import { WsRoomUserModel } from "@/models/common/User";
-import { RoomItem } from "@/models/common/Room";
+import { ChatResponse, RoomItem } from "@/models/common/Room";
 
 interface RoomInfoState {
-    room: RoomItem | null;
-    setRoom: (room: any) => void;
-    updateUsers: (users: WsRoomUserModel[]) => void;
-    clearRoom: () => void;
+  room: RoomItem | null;
+  setRoom: (room: any) => void;
+  chats: ChatResponse[];
+  updateUsers: (users: WsRoomUserModel[]) => void;
+  pushChat: (value: RoomInfoState["chats"][number]) => void;
+  clearChat: () => void;
+  clearRoom: () => void;
 }
 
 export const useRoomInfoStore = create<RoomInfoState>((set) => ({
-    room: null,
-    setRoom: (room) => set({ room }),
-    updateUsers: (users) => set((state) => state.room ? { room: {...state.room, users } }: {}),
-    clearRoom: () => set({ room: null }),
+  room: null,
+  setRoom: (room) => set({ room }),
+  chats: [],
+  updateUsers: (users) => set((state) => state.room ? { room: { ...state.room, users } } : {}),
+  pushChat: (value) => set((state) => {
+    let newChats: RoomInfoState["chats"];
+    if (state.chats.length >= 100) {
+      newChats = update(state.chats, {
+        $splice: [[0, 1]],
+        $push: [value],
+      });
+    } else {
+      newChats = update(state.chats, { $push: [value] });
+    }
+    return { chats: newChats };
+  }),
+  clearChat: () => set({ chats: [] }),
+  clearRoom: () => set({ room: null }),
 }));
