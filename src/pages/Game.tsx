dcf896edafe6f0fbda8053d0bd9ae5c0f554dc91
Ryu-ga <frech5679@icloud.com>
2025-06-10 01:34:52 +0900

    flip -> isRotated 변경

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -74,8 +74,8 @@ const GamePage: React.FC = () => {
 		return [left, right];
 	}, [game?.players]);
 
-	const [flip, setFlip] = useState<number>(0);
-	const flipInterval = useRef<NodeJS.Timeout | null>(null);
+	const [isRotated, setIsRotated] = useState<number>(0);
+	const rotateInterval = useRef<NodeJS.Timeout | null>(null);
 	const [activeItem, setActiveItem] = useState<string | null>(null);
 
 	// 현재 드래그중인 카드 오버레이
@@ -84,7 +84,14 @@ const GamePage: React.FC = () => {
 		if (activeItem.startsWith(CARD_PREFIX)) {
 			const assetId = Number.parseInt(activeItem.slice(CARD_PREFIX.length));
 			if (assetId > 0 && assetId < 60) {
-				return <RouteCard id="overlay" assetId={assetId} isDraggable={false} />;
+				return (
+					<RouteCard
+						id="overlay"
+						assetId={assetId}
+						isDraggable={false}
+						isRotated={isRotated}
+					/>
+				);
 			}
 			if (assetId > 100 && assetId < 120) {
 				return (
@@ -93,32 +100,32 @@ const GamePage: React.FC = () => {
 			}
 		}
 		return null;
-	}, [activeItem, flip]);
+	}, [activeItem, isRotated]);
 
-	// 드래그 시작: 오버레이와 flip 애니메이션 설정
+	// 드래그 시작: 오버레이와 rotate 애니메이션 설정
 	const handleDragStart = (e: DragStartEvent) => {
 		const id = e.active.id.toString();
 		toast(`드래그 시작: ${id}`);
 		setActiveItem(id);
-		setFlip(0);
+		setIsRotated(0);
 
-		if (id.startsWith(CARD_PREFIX) && !flipInterval.current) {
-			flipInterval.current = setInterval(
-				() => setFlip((prev) => (prev + 1) % 2),
+		if (id.startsWith(CARD_PREFIX) && !rotateInterval.current) {
+			rotateInterval.current = setInterval(
+				() => setIsRotated((prev) => (prev + 1) % 2),
 				1500,
 			);
 		}
 	};
 
-	// 드래그 종료: 카드 드랍 처리 및 flip 인터벌 해제
+	// 드래그 종료: 카드 드랍 처리 및 rotate 인터벌 해제
 	const handleDragEnd = (e: DragEndEvent) => {
 		const overId = e.over?.id.toString();
 		const activeId = e.active.id.toString();
 
-		// flip 반복 종료
-		if (flipInterval.current) {
-			clearInterval(flipInterval.current);
-			flipInterval.current = null;
+		// rotate 반복 종료
+		if (rotateInterval.current) {
+			clearInterval(rotateInterval.current);
+			rotateInterval.current = null;
 		}
 
 		if (activeId.startsWith(CARD_PREFIX)) {
@@ -135,7 +142,7 @@ const GamePage: React.FC = () => {
 				const row = Number(rowStr);
 				const col = Number(colStr);
 
-				if (cardId > 0 && cardId < 60) usePathCard(cardId, row, col, flip);
+				if (cardId > 0 && cardId < 60) usePathCard(cardId, row, col, isRotated);
 				else if (cardId === 107) useRockfallCard(cardId, row, col);
 
 				// 골셀에 드랍 (지도카드)
